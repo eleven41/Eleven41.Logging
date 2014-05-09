fc97087e1e8383740ce4132b79648dc642cc17f3
mwhouser <matt@eleven41.com>
2014-05-09 08:57:39 -0400

    Bump version to 1.1.1

--- Eleven41.Logging/Properties/AssemblyInfo.cs
+++ Eleven41.Logging/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Eleven41.Logging")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Eleven41 Software")]
+[assembly: AssemblyCompany("Eleven41 Software Inc.")]
 [assembly: AssemblyProduct("Eleven41.Logging")]
-[assembly: AssemblyCopyright("Copyright © 2013, Eleven41 Software")]
+[assembly: AssemblyCopyright("Copyright © 2014, Eleven41 Software Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
